499f971706c8efa2c173bb0a1d36952bfc40ac8b
Rishabh Verma <aryanverma2412@gmail.com>
2021-02-10 00:12:47 +0530

    Last Commit

---
+++ src/requests.js
@@ -0,0 +1,18 @@
+  
+const API_KEY = '013072d403296199dafe4796e6c418f0';
+
+const requests = {
+	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
+	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+};
+
+export default requests;
+
+ //api key 013072d403296199dafe4796e6c418f0
+    // api type https://api.themoviedb.org/3/movie/550?api_key=013072d403296199dafe4796e6c418f0
